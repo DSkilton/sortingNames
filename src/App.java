56cff1db2e388daa42c807de1f53a936b4909852
DSkilton <the_skiltons@hotmail.com>
2019-01-06 17:05:56 +0000

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+         String[] actualNames = new String[]{
+                "Jonhson", "Wilson",
+                "Wilkinson", "Abraham", "Dagobert"};
+        Arrays.sort(actualNames);
+        for (final String name : actualNames){
+            System.out.println(name);
+        }
+    }
+
+}
